abe95d8d125a4b94a6260d77150d4812dec0a1d0
klover <49548817+klover2@users.noreply.github.com>
2024-04-10 13:02:08 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: "xxl-job-demo",
     script: 'java',
-    args: '-jar /root/workspace/java-demo/xxl-job-demo/xxl-job-demo-1.0.0.jar',
+    args: '-jar /root/workspace/java-demo/xxl-job-demo/target/xxl-job-demo-1.0.0.jar',
     instances: 1,
     exec_mode: 'fork',
     watch: true,
