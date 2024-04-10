cdef4606cc7713ed5951ccf1347efdc8ebe82ad7
klover <49548817+klover2@users.noreply.github.com>
2024-04-10 13:29:09 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,5 @@ module.exports = {
     instances: 1,
     exec_mode: 'fork',
     watch: true,
-    max_memory_restart: '100M', // 最大内存限制数，超出自动重启
-    min_uptime: '60s', // 应用运行少于时间被认为是异常启动
-    max_restarts: 3, // 最大异常重启次数，即小于min_uptime运行时间重启次数；
   }]
 };
